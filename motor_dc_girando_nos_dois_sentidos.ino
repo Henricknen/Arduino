e812a812de286b663eb1258254b988e3bc92cd5d
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-12-28 13:18:30 -0300

    ultiizando_circuito_L293D_de_ponte_'H_dupla'_que_consegue_controlar_ate_dois motores_em_sentido_diferentes

--- motor_dc_girando_nos_dois_sentidos.ino
+++ motor_dc_girando_nos_dois_sentidos.ino
@@ -1,55 +1,34 @@
-#define velm1 3		// 'define' velocidade do motor 1 ligada na porta 'pwm 3'
-#define m1a 4			// 'moto1 a' está na porta 2
-#define m1b 5				// 'moto1 b' está na porta 4
-#define velm2 11
-#define m2a 12
-#define m2b 13
-#define tmp 5000	// definição de tempo para icar girando
+#define velmotor 3		// 'define' ligado na 'pwm' 3
+#define m1a 2
+#define m1b 4
+#define tmp 5000
 
-int vel = 255;	// velocidade do giro
+int vel = 255;		// variável xontém a veloçidade do motor
 
-void setup(){
-  pinMode(velm1, OUTPUT);	// definição das porta como saida
+void setup() {		// configurações iniçiais
+  pinMode(velmotor, OUTPUT);
   pinMode(m1a, OUTPUT);
   pinMode(m1b, OUTPUT);
-  pinMode(velm2, OUTPUT);
-  pinMode(m2a, OUTPUT);
-  pinMode(m2b, OUTPUT);
   
-  
-  
-  analogWrite(velm1, 0);		// iniçializando tudo com '0'
-  analogWrite(m1a, LOW);
-  analogWrite(m1b, LOW);
-  analogWrite(velm2, 0);		// iniçializando tudo com '0'
-  analogWrite(m2a, LOW);
-  analogWrite(m2b, LOW);
-
+  digitalWrite(m1a,LOW);
+  digitalWrite(m1b,LOW);
+  analogWrite(velmotor,vel);
 }
 
-void loop(){
-  //M1
-  analogWrite(velm1, vel);
-  analogWrite(m1a, HIGH);		// pino em 'HIGH' e
-  analogWrite(m1b, LOW);			// o outro em 'LOW' fará o motor girar em um sentido
-  //M2
-  analogWrite(velm2, vel);
-  analogWrite(m2a, LOW);		// pino em 'HIGH' e
-  analogWrite(m2b, HIGH);			// o outro em 'LOW' fará o motor girar em um sentido
-
-  delay(tmp);
+void loop() {
+  vel=255;
+  analogWrite(velmotor,vel);
   
-  analogWrite(velm1, 0);
-  analogWrite(m1a, LOW);		// 'LOW' nos dois pinos fará o motor para antes de inverter o sentido
-  analogWrite(m1b, LOW);
+  digitalWrite(m1a,LOW);
+  digitalWrite(m1b,HIGH);
+  delay(tmp);
+
+  digitalWrite(m1a,LOW);		// parando o motor por um tempo muito pequeno
+  digitalWrite(m1b,LOW);
   delay(100);
 
-  //M1
-  analogWrite(m1a, LOW);		//fará girar o motor em sentido diferente
-  analogWrite(m1b, HIGH); 
-  //M2
-  analogWrite(m2a, HIGH);		//fará girar o motor em sentido diferente
-  analogWrite(m2b, LOW); 
+  digitalWrite(m1a,HIGH);		// trocando o sentido de giro
+  digitalWrite(m1b,LOW);
   delay(tmp);
 
 }
